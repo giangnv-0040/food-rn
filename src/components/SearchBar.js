68a9b65d3ea7d69b8fbc74d21e11610cfbf926f0
giangnv-0040 <nguyen.van.giang@sun-asterisk.com>
2020-08-12 13:26:44 +0700

    Lesson 8: A touch more styling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,22 +1,36 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
 const SearchBar = () => {
     return (
         <View style={styles.background}>
-            <Feather name="search" size={30} />
-            <Text>SearchBar</Text>
+            <Feather name="search" style={styles.iconStyle} />
+            <TextInput style={styles.inputStyle} placeholder="Search"/>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     background: {
+        marginTop: 15,
         backgroundColor: '#F0EEEE',
         height: 50,
         borderRadius: 5,
-        marginHorizontal: 15
+        marginHorizontal: 15,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    inputStyle: {
+        // borderColor: 'black',
+        // borderWidth: 1,
+        flex: 1,
+        fontSize: 18
+    },
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: 'center',
+        marginHorizontal: 10
     }
 });
 
